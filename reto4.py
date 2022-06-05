72b30aaceaed4a192713738513f86aab23689213
upoison <destivengarzon@gmail.com>
2022-06-04 23:57:33 -0500

    Adding new info

--- reto4.py
+++ reto4.py
@@ -1,6 +1,7 @@
 def ordenes(rutinaContable):
     from functools import reduce
-    ListaTotales = list(map(lambda x: [x[0]]+ list(map(lambda z: z[1]*z[2] ,x[1:])), rutinaContable))
+    
+    list(map(lambda x: [x[0]]reduce ListaTotales =+ list(map(lambda z: z[1]*z[2] ,x[1:])), rutinaContable))
     ListaTotales = list(map(lambda x: [x[0]] + [reduce(lambda a,e:round(a+e, 2) , x[1:])], ListaTotales))
     print(ListaTotales)
 
@@ -11,7 +12,7 @@ def ordenes(rutinaContable):
     print('----------------- Inicio Registro diario --------------------------')
     print('----------------- Fin Registro diario -----------------------------')
 
-rutinaContable = [    
+rutinaContable = [
     [1201, ("5464", 4, 25842.99), ("7854",18,23254.99), ("8521", 9, 48951.95)],
     [1202, ("8756", 3, 115362.58),("1112",18,2354.99)],
     [1203, ("2547", 1, 125698.20), ("2635", 2, 135645.20), ("1254", 1, 13645.20),("9965", 5, 1645.20)],
